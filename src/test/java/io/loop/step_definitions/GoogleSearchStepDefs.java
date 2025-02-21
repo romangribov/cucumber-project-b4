724c142f3ae240b50c46a812687d3c80fab70469
Roam <roman.gribov94@gmail.com>
2025-02-21 06:22:58 +0300

    google_search_outline, 8 scenarios day13,

--- src/test/java/io/loop/step_definitions/GoogleSearchStepDefs.java
+++ src/test/java/io/loop/step_definitions/GoogleSearchStepDefs.java
@@ -10,10 +10,12 @@ import io.loop.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
@@ -30,17 +32,12 @@ public class GoogleSearchStepDefs {
     @When("user types Loop Academy in the google search box and clicks enter")
     public void user_types_loop_academy_in_the_google_search_box_and_clicks_enter() throws InterruptedException {
         googleSearchPage.searchBox.sendKeys("Loop Academy" + Keys.ENTER);
-
         WebElement element = Driver.getDriver().findElement(By.xpath("//iframe[@title='reCAPTCHA']"));
-
         Driver.getDriver().switchTo().frame(element);
-
         if(googleSearchPage.captcha.isDisplayed()){
             googleSearchPage.captcha.click();
         }
-
         Driver.getDriver().switchTo().defaultContent();
-
         Thread.sleep(5000);
     }
 
@@ -67,5 +64,42 @@ public class GoogleSearchStepDefs {
         wait.until(ExpectedConditions.titleIs(expectedTitle));
         String actualTitle = Driver.getDriver().getTitle();
         assertEquals("Expected result does not match the actual", expectedTitle, actualTitle);
+    }
+
+    @Then("user searches the following items")
+    public void user_searches_the_following_items(List<String> items) {
+//        for (String item : items) {
+//            googleSearchPage.searchBox.clear();
+//            googleSearchPage.searchBox.sendKeys(item + Keys.ENTER);
+//            googleSearchPage.handleCaptcha(Driver.getDriver(), googleSearchPage.captcha);
+//            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+//            wait.until(ExpectedConditions.titleIs(item + " - Google Search"));
+//            assertEquals("Expected does not match the actual",item + " - Google Search",Driver.getDriver().getTitle());
+//        }
+
+        items.forEach(p-> {
+            googleSearchPage.searchBox.clear();
+            googleSearchPage.searchBox.sendKeys(p + Keys.ENTER);
+            googleSearchPage.handleCaptcha(Driver.getDriver(), googleSearchPage.captcha);
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.titleIs(p + " - Google Search"));
+            assertEquals("Expected does not match the actual",p + " - Google Search",Driver.getDriver().getTitle());
+        });
+
+    }
+
+    @Then("we love Loop Academy")
+    public void we_love_loop_academy() {
+        System.out.println("We Love Loop Academy");
+    }
+
+    @When("user search for {string}")
+    public void user_search_for(String country) {
+        googleSearchPage.searchBox.sendKeys("What is the capital of " + country +Keys.ENTER);
+    }
+    @Then("user should see the {string} in the results")
+    public void user_should_see_the_in_the_results(String capital) {
+        googleSearchPage.handleCaptcha(Driver.getDriver(), googleSearchPage.captcha);
+        assertEquals("Expected capital city: " + capital + " does not match with actual one: " + googleSearchPage.capital.getText(), capital, googleSearchPage.capital.getText());
     }
 }
